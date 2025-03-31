6cccb9362e6c3874a7a7aa4cf1ec430af0c3e377
0Vipin0 <vipinmalik941@gmail.com>
2025-03-31 20:47:30 +0530

    Initial Commit

---
+++ bin/backlog_manager_excel.dart
@@ -0,0 +1,5 @@
+import 'package:backlog_manager_excel/backlog_manager_excel.dart' as backlog_manager_excel;
+
+void main(List<String> arguments) {
+  print('Hello world: ${backlog_manager_excel.calculate()}!');
+}
---
+++ test/backlog_manager_excel_test.dart
@@ -0,0 +1,8 @@
+import 'package:backlog_manager_excel/backlog_manager_excel.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
